b77a2abcaa56112824524889e3de107f33fbe7e0
Kushagra5678 <kushu.7feb@gmail.com>
2020-10-11 17:42:09 +0530

    modify: add 13 nodes to BST

--- src/test/java/binarySearchTree/BSTTest.java
+++ src/test/java/binarySearchTree/BSTTest.java
@@ -1,16 +1,28 @@
 package binarySearchTree;
+
 import org.junit.*;
 
 import com.practice.binarysearchtree.BST;
+
 public class BSTTest {
 	@Test
-	public void given3NumbersWhenAddedToBinaryTreeShouldReturnSizeThree() {
-		BST binarySearchTree=new BST<>();
+	public void given13NumbersWhenAddedToBinaryTreeShouldReturnSize13() {
+		BST binarySearchTree = new BST<>();
 		binarySearchTree.add(56);
 		binarySearchTree.add(30);
 		binarySearchTree.add(70);
-		
-		int size=binarySearchTree.getSize();
-		Assert.assertTrue(size==3);
+		binarySearchTree.add(22);
+		binarySearchTree.add(40);
+		binarySearchTree.add(60);
+		binarySearchTree.add(95);
+		binarySearchTree.add(11);
+		binarySearchTree.add(65);
+		binarySearchTree.add(3);
+		binarySearchTree.add(16);
+		binarySearchTree.add(63);
+		binarySearchTree.add(67);
+
+		int size = binarySearchTree.getSize();
+		Assert.assertTrue(size == 13);
 	}
 }
